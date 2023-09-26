c9f03e368f1b6b8e8efaafba9e7a901269b453e4
bebi21 <nguyenlocnd18@gmail.com>
2023-09-26 14:27:12 +0700

    thay  doi  lan 1

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let listOrder = document.getElementById("listOrder");
+let currentUser = localStorage.getItem("currentUser");
+if (currentUser == null) {
+  window.location.href = "login.html";
+}
+
 /* let listItem = [
   {
     id: 1,
@@ -78,21 +83,37 @@ function render() {
 render();
 
 // function  mua hang
+/* 
+các bước làm khi  đi  chợ ,  mua hàng
 
+b1 Lấy giỏ hàng
+
+dựa  vào curentUser lưu  trên  local 
+
+b2 lấy
+
+
+*/
 function buy(id) {
-  let currentUser = localStorage.getItem("currentUser");
+  //  lấy tên người dùng trên  local
+
+  // product ở đây  là lưu  sản phẩm mà người dùng muốn mua căn cứ theo  ID của sản phẩm
   let product;
+  // users ở đây là  danh  sách người dùng đã tạo  tải khoản thành công
   let users = JSON.parse(localStorage.getItem("users"));
+  // vòng for ở đây dùng để lấy ra sản phẩm mà người dùng chọn  (để bỏ vào giỏ của người dùng)
   for (let i = 0; i < arrListItem.length; i++) {
     if (arrListItem[i].id == id) {
       product = arrListItem[i];
       break;
     }
   }
-
+  // vòng for ở đây duyệt mảng users  khi đăng kí kiểm tra xem cái tên (currentUser)
+  // của người dùng login trùng với cái username trong mảng users
   for (let i = 0; i < users.length; i++) {
     if (users[i].userName == currentUser) {
       // giỏ hàng là cái này   users[i].cart
+      //biến check kiểm tra
       let check = true;
       for (let j = 0; j < users[i].cart.length; j++) {
         //   cái này để lọc  qua giỏ hàng nếu không có thì push vào giỏ hàng
@@ -107,6 +128,9 @@ function buy(id) {
           break;
         }
       }
+      // nếu biến check là true thì chưa có sản phẩm
+      // nếu là false thì đã có sản phẩm và tăng  sản phẩm lên 1 lần (đã tăng  lên  ở dòng 119)
+      // và không chạy
       if (check) {
         let productDetail = { ...product, quantity: 1 };
         users[i].cart.push(productDetail);
